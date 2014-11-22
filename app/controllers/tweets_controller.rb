d0b2d41a45fb06cca18d79d338a47bdaef5b102f
dnguyen1193 <dnguyen1193@gmail.com>
2014-11-22 14:46:26 -0500

    added tweets list and link

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,12 +3,16 @@ class TweetsController < ApplicationController
 		#creates new instance of the object Tweet
 		#'@' -> means the view has an access to the variable
 		@tweet = Tweet.new
+		@tweets = Tweet.all
+		# this variable will hold all the tweets
 	end
 
 	# create doesn't have access to the object Tweet since it's not defined here
 	def create
 		#saves it to database
 		@tweet = Tweet.create(tweet_params)
+		# give create an instance of all the tweets too -> or it can't render
+		@tweets = Tweet.all
 		flash[:success] = "You have successfully created a Tweet!"
 		#it doesn't need to be rendered on a new page -> just on the same page as the form, the new page
 		render 'new'
